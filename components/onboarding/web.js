3dfa9c7f4584448ccbce6db92b753770f0e5fd54
Adeoluwa <Oliyideadeoluwajohn1@gmail.com>
2024-05-13 09:40:34 +0100

    Updated the app

--- components/onboarding/web.js
+++ components/onboarding/web.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 const Web = () => {
+  const [isLoading, setIsLoading] = useState(true); 
+
   return (
     <SafeAreaView style={styles.container}>
+      <Spinner
+        visible={isLoading}
+        textContent={'Loading...'}
+        textStyle={styles.spinnerText}
+      />
+
       <WebView
         source={{ uri: 'https://smmleet.com/login' }}
         style={styles.webview}
         scalesPageToFit={true}
+        onLoad={() => setIsLoading(false)}
       />
     </SafeAreaView>
   );
@@ -22,6 +32,9 @@ const styles = StyleSheet.create({
   webview: {
     flex: 1,
     marginTop: 0,
+  },
+  spinnerText: {
+    color: '#fff',
   },
 });
 
